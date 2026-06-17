b3a8370dcafcd8802ed187edf84f7f8de3d99857
raychelleKalekye <kalekye.raychelle@strathmore.edu>
2026-06-17 14:59:10 +0300

    Develop the Acts covered page - Added heading to acts page and its style ref:#69

---
+++ frontend/src/Components/ActsHeading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/ActsHeading.css'
+const ActsHeading = () => {
+  return (
+    <div className="ActsHeading">
+      <h1>Acts Covered</h1>
+      <p>Uhaki draws answers from these 20 Kenyan acts and the Constitution. Browse by category to see what questions you can ask.</p>
+    </div>
+  )
+}
+
+export default ActsHeading
+
--- frontend/src/Pages/ActsCovered.js
+++ frontend/src/Pages/ActsCovered.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ActsHeader from '../Components/ActsHeader';
+import ActsHeading from '../Components/ActsHeading';
 
 const ActsCovered = () => {
   return (
     <>
     <ActsHeader/>
+    <ActsHeading/>
     <div className="acts-covered">
 
     </div>
