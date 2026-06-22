00210eb22938332ecea6fb4595f826bad0cd55f4
raychelleKalekye <kalekye.raychelle@strathmore.edu>
2026-06-22 15:27:10 +0300

    Add langauage context - Created language context ref:#77

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import '../Styles/Header.css';
 import { IconScale } from '@tabler/icons-react';
 import { IconRefresh } from '@tabler/icons-react';
-
+import { useLanguage } from '../Context/LanguageContext';
 const Header = ({ onClearChat }) => {
   
+  const {lang, setLang}=useLanguage();
   return (  
       <div className='header'>
         <div className="chatLeft">
@@ -22,13 +23,23 @@ const Header = ({ onClearChat }) => {
         <a href='/ActsCovered'>Acts Covered</a>
         <button onClick={onClearChat} className='clear-btn'><IconRefresh size={16}/> Clear Chat</button>
 
-         <div className='language'>
-                <button className='lang-btn1'>EN</button>
-                <button className='lang-btn'>SW</button>
+          <div className='language'>
+          <button
+            className={`lang-btn1 ${lang === 'en' ? 'lang-active' : ''}`}
+            onClick={() => setLang('en')}
+          >
+            EN
+          </button>
+          <button
+            className={`lang-btn ${lang === 'sw' ? 'lang-active' : ''}`}
+            onClick={() => setLang('sw')}
+          >
+            SW
+          </button>
         </div>
       </div>
     </div>
   );
 };
-
+ 
 export default Header;
